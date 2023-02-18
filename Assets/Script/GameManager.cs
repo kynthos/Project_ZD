faee66539ff18f1308319458aa313113a98c1bf9
2Jaebeen <daniluna111@gmail.com>
2023-02-18 17:27:23 +0900

    서브 모듈 및 프레임 워크 작업 시작

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestCode : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
+    protected override void Init()
+    {
+        //throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
